ed2ae967c2e0075705c21a0f842239f9c5e271cc
swapan <swapan.d83@gmail.com>
2021-05-08 20:33:28 +0600

    Show more earthquake data in the list

---
+++ app/src/main/java/android/example/com/earthquakeapinetworking/Earthquake.java
@@ -0,0 +1,47 @@
+package android.example.com.earthquakeapinetworking;
+
+/**
+ * An {@link Earthquake} object contains information related to a single earthquake.
+ */
+public class Earthquake {
+
+    /** Magnitude of the earthquake */
+    private String mMagnitude;
+
+    /** Location of the earthquake */
+    private String mLocation;
+
+    /** Date of the earthquake */
+    private String mDate;
+
+    /**
+     * Constructs a new {@link Earthquake} object.
+     *
+     * @param magnitude is the magnitude (size) of the earthquake
+     * @param location is the city location of the earthquake
+     * @param date is the date the earthquake happened
+     */
+    public Earthquake(String magnitude, String location, String date) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mDate = date;
+    }
+    /**
+     * Returns the magnitude of the earthquake.
+     */
+    public String getmMagnitude() {
+        return mMagnitude;
+    }
+    /**
+     * Returns the location of the earthquake.
+     */
+    public String getmLocation() {
+        return mLocation;
+    }
+    /**
+     * Returns the date of the earthquake.
+     */
+    public String getmDate() {
+        return mDate;
+    }
+}
