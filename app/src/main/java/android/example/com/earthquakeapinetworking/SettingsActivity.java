c1aba540395dd9f16752a89eb35505a4d99c14a6
swapan <swapan.d83@gmail.com>
2021-05-31 07:36:26 +0600

    Display preference summaries

--- app/src/main/java/android/example/com/earthquakeapinetworking/SettingsActivity.java
+++ app/src/main/java/android/example/com/earthquakeapinetworking/SettingsActivity.java
@@ -3,8 +3,11 @@ package android.example.com.earthquakeapinetworking;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -13,12 +16,32 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
-    public static class EarthquakePreferenceFragment extends PreferenceFragment{
+    public static class EarthquakePreferenceFragment extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
 
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMagnitude);
+        }
+
+
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences =
+                    PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(),"");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 }
